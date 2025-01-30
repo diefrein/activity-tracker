24e2628dfd802240eb97d014a6e33174f00d2402
Kirill <kirill.ustimencko@gmail.com>
2025-01-30 23:37:45 +0300

    form for creating user profile

---
+++ src/bot/storage/states.py
@@ -0,0 +1,10 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class UserForm(StatesGroup):
+    name = State()
+    weigth = State()
+    heigth = State()
+    age = State()
+    activity_minutes = State()
+    city = State()
+    calory_target = State()
